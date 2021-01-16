1b5597e2ea1b0cb92d1c745d17cbc805d79cacb2
yonatanleulseged <yleulseged@gmail.com>
2021-01-15 21:35:18 -0800

    fix remart logo not showing up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,43 +23,57 @@ function Navbar() {
     }, []);
 
     window.addEventListener('resize', showButton);
-  return (
-    <>
-      <nav className='navbar'>
-        <div className='nav-bar-container'>
-          <Link to="/" className='navbar-logo' onClick = {closeMobileMenu}>
-            Remart <i className='fab fa-typo3' />
-          </Link>
-          <div className='menu-icon' onClick = {handleClick}>
-              <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
+    return (
+      <>
+        <nav className='navbar'>
+          <div className='navbar-container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              Remart
+              <i class='fab fa-typo3' />
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/services'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/products'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Products
+                </Link>
+              </li>
+  
+              <li>
+                <Link
+                  to='/sign-up'
+                  className='nav-links-mobile'
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
           </div>
-          <ul className = {click ? 'nav-menu active' : 'nav-menu'}> 
-          <li className = 'nav-item'> 
-            <Link to = '/' className = 'nav-links' on Click= {closeMobileMenu}>
-                Home
-            </Link>
-          </li>
-          <li className = 'nav-item'> 
-            <Link to = '/services' className = 'nav-links' on Click= {closeMobileMenu}>
-                Services
-            </Link>
-          </li>
-          <li className = 'nav-item'> 
-            <Link to = '/products' className = 'nav-links' on Click= {closeMobileMenu}>
-                Products
-            </Link>
-          </li>
-          <li className = 'nav-item'> 
-            <Link to = '/sign-up' className = 'nav-links-mobile' on Click= {closeMobileMenu}>
-                Sign Up
-            </Link>
-          </li>
-          </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-        </div>
-      </nav>
-    </>
-  );
+        </nav>
+      </>
+    );
 }
 
 export default Navbar;
