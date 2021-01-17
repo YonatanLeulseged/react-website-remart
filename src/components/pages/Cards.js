cf54201f004770b56c4610e4ef1b6a594cd70762
yonatanleulseged <yleulseged@gmail.com>
2021-01-17 15:48:14 -0800

    added more cards

--- src/components/pages/Cards.js
+++ src/components/pages/Cards.js
@@ -21,6 +21,20 @@ function Cards() {
               label="Adventure"
               path="/services"
             />
+
+            <CardItem
+              src='images/img-4.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-8.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+              />
+              
           </ul>
         </div>
       </div>
